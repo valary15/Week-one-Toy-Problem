2927c8bc543bab8c2de2f816d6338b9090e30b69
valary <valaryoluoch@gmail.com>
2024-07-09 10:57:35 +0300

    Reviewed

--- netSalaryCalculator.js
+++ netSalaryCalculator.js
@@ -18,41 +18,39 @@ function calculatePAYE(grossPay) {
 
 function calculateNHIF(grossPay) {
   if (grossPay <= 5999) {
-    return grossPay;
-  } else if (grossPay >= 6000 && grossPay <= 7999) {
+    return 150;
+  } else if (grossPay <= 7999) {
     return 300;
-  } else if (grossPay >= 8000 && grossPay <= 11999) {
+  } else if (grossPay <= 11999) {
     return 400;
-  } else if (grossPay >= 12000 && grossPay <= 14999) {
+  } else if (grossPay <= 14999) {
     return 500;
-  } else if (grossPay >= 15000 && grossPay <= 19999) {
+  } else if (grossPay <= 19999) {
     return 600;
-  } else if (grossPay >= 20000 && grossPay <= 24999) {
+  } else if (grossPay <= 24999) {
     return 750;
-  } else if (grossPay >= 25000 && grossPay <= 29999) {
+  } else if (grossPay <= 29999) {
     return 850;
-  } else if (grossPay >= 30000 && grossPay <= 34999) {
+  } else if (grossPay <= 34999) {
     return 900;
-  } else if (grossPay >= 35000 && grossPay <= 39999) {
+  } else if (grossPay <= 39999) {
     return 950;
-  } else if (grossPay >= 40000 && grossPay <= 44999) {
+  } else if (grossPay <= 44999) {
     return 1000;
-  } else if (grossPay >= 45000 && grossPay <= 49999) {
+  } else if (grossPay <= 49999) {
     return 1100;
-  } else if (grossPay >= 50000 && grossPay <= 59999) {
+  } else if (grossPay <= 59999) {
     return 1200;
-  } else if (grossPay >= 60000 && grossPay <= 69999) {
+  } else if (grossPay <= 69999) {
     return 1300;
-  } else if (grossPay >= 70000 && grossPay <= 79999) {
+  } else if (grossPay <= 79999) {
     return 1400;
-  } else if (grossPay >= 80000 && grossPay <= 89999) {
+  } else if (grossPay <= 89999) {
     return 1500;
-  } else if (grossPay >= 90000 && grossPay <= 99999) {
+  } else if (grossPay <= 99999) {
     return 1600;
-  } else if (grossPay >= 100000) {
-    return 1700;
   } else {
-    return 0;
+    return 1700;
   }
 }
 
@@ -66,18 +64,15 @@ function calculateNSSF(grossPay) {
   return nssfDeduction;
 }
 
-function calculateNestSalary(salary) {
+function calculateNetSalary(salary) {
   const payeDeduction = calculatePAYE(salary);
   const nhifDeduction = calculateNHIF(salary);
   const nssfDeduction = calculateNSSF(salary);
 
-  const totalDeductions = payeDeduction - nhifDeduction - nssfDeduction;
-
+  const totalDeductions = payeDeduction + nhifDeduction + nssfDeduction;
   const netIncome = salary - totalDeductions;
 
   return netIncome;
 }
 
-console.log(calculateNestSalary(100000));
-
-
+console.log(calculateNetSalary(200000));
